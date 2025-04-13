93fa2ebd75d0881243c6bbd533abf694244e710c
Luiz Carlos <103046399+luizaraujo18@users.noreply.github.com>
2025-04-13 11:23:30 -0300

    Delete 04_Inserindo_alunos_na_tabela.sql

--- 04_Inserindo_alunos_na_tabela.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO tb_alunos
-    (nome, idade, data_matricula, status) 
-VALUES 
-    ('João Silva', 20, '2023-02-01', 'Ativo'), 
-    ('Maria Oliveira', 22, '2022-08-15', 'Ativo'), 
-    ('Pedro Costa', 19, '2021-09-30', 'Formado');
