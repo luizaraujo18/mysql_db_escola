d433ea2c1eefaadab5dfe19b0c98ef955bb783ae
Luiz Carlos <103046399+luizaraujo18@users.noreply.github.com>
2025-04-13 10:32:05 -0300

    Update 04_Inserindo_alunos_na_tabela.sql

--- 04_Inserindo_alunos_na_tabela.sql
+++ 04_Inserindo_alunos_na_tabela.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_alunos  
-(nome, idade, data_matricula, status) 
+INSERT INTO tb_alunos
+    (nome, idade, data_matricula, status) 
 VALUES 
-('João Silva', 20, '2023-02-01', 'Ativo'), 
-('Maria Oliveira', 22, '2022-08-15', 'Ativo'), 
-('Pedro Costa', 19, '2021-09-30', 'Formado');
+    ('João Silva', 20, '2023-02-01', 'Ativo'), 
+    ('Maria Oliveira', 22, '2022-08-15', 'Ativo'), 
+    ('Pedro Costa', 19, '2021-09-30', 'Formado');
