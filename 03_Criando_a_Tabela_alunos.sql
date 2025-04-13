f2921f824b8176370e8bd66ee10aefb6247a1326
Luiz Carlos <103046399+luizaraujo18@users.noreply.github.com>
2025-04-13 10:31:19 -0300

    Update 03_Criando_a_Tabela_alunos.sql

--- 03_Criando_a_Tabela_alunos.sql
+++ 03_Criando_a_Tabela_alunos.sql
@@ -1,1 +1,7 @@
-SELECT * FROM tb_
+CREATE TABLE IF NOT EXISTS tb_alunos (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    nome VARCHAR(150) NOT NULL, 
+    idade INT NOT NULL, 
+    data_matricula DATE NOT NULL, 
+    status VARCHAR(20) NOT NULL 
+); 
