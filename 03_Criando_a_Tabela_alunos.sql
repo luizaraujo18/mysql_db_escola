2a11bb5e30cee04b122f563380b9555e67d0d9e6
luizaraujo18 <luizaraujo_18@outlook.com>
2025-04-13 10:28:55 -0300

    atv04

--- 03_Criando_a_Tabela_alunos.sql
+++ 03_Criando_a_Tabela_alunos.sql
@@ -1,7 +1,1 @@
-CREATE TABLE IF NOT EXISTS tb_alunos ( 
-	id INT PRIMARY KEY AUTO_INCREMENT, 
-	nome VARCHAR(150) NOT NULL, 
-	idade INT NOT NULL, 
-	data_matricula DATE NOT NULL, 
-	status VARCHAR(20) NOT NULL 
-);
+SELECT * FROM tb_
---
+++ 04_Inserindo_alunos_na_tabela.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_alunos  
+(nome, idade, data_matricula, status) 
+VALUES 
+('João Silva', 20, '2023-02-01', 'Ativo'), 
+('Maria Oliveira', 22, '2022-08-15', 'Ativo'), 
+('Pedro Costa', 19, '2021-09-30', 'Formado');
