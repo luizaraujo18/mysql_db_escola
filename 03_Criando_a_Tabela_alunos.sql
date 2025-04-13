5bbbff5dfcac303c35bd9cd942a7f711fb00847b
Luiz Carlos <103046399+luizaraujo18@users.noreply.github.com>
2025-04-13 11:23:19 -0300

    Delete 03_Criando_a_Tabela_alunos.sql

--- 03_Criando_a_Tabela_alunos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS tb_alunos (
-    id INT PRIMARY KEY AUTO_INCREMENT, 
-    nome VARCHAR(150) NOT NULL, 
-    idade INT NOT NULL, 
-    data_matricula DATE NOT NULL, 
-    status VARCHAR(20) NOT NULL 
-); 
